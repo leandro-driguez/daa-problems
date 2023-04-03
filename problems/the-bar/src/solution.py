12c86217c8f1d9185052f7d772e868e4210ac81d
leandro-driguez <leandro__rodriguez@outlook.com>
2023-04-03 00:30:31 -0400

    fix: add the odd cases to the tester

--- problems/the-bar/src/solution.py
+++ problems/the-bar/src/solution.py
@@ -1,5 +1,5 @@
 from typing import List
-from math import ceil, floor
+from math import floor
 
 def exist_k(ranges: List[int], N : int) :
     oPar = 0
@@ -15,8 +15,8 @@ def solve(a: List[int]):
 
     if a[N-1] >= 0: 
         return N if sum(a) > 0 else -1
-    
-    # accumulated sum of the hwole list
+
+    # accumulated sum of the whole list
     accu_sum = sum(a)
 
     ranges = [0 for _ in range(N+2)]
@@ -25,20 +25,11 @@ def solve(a: List[int]):
 
     for i in range(N - N//2):
         # accumulated sum of the last i elements of the list
-        print(i)
         if accu_sum <= 0:
             j = min(N//2, floor(accu_sum / a[N-1]))
-
             ranges[N - i - j] += 1
             ranges[N + 1 - i] -= 1
-            
-            print(ranges)
+
         accu_sum -= a[i]
 
     return exist_k(ranges, N)
-
-
-example = [-1, 8, -2, 3, -2, -2, -2, -2]
-print(solve(example))
-example = [-1, 6, -2, 3, -2, -2, -2]
-print(solve(example))
