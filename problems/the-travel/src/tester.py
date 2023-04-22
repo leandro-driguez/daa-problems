8f547fe4bd899289562f5b14462f97db72b80056
Marcos <marcosmath44@gmail.com>
2023-04-22 01:23:56 -0400

    Update tester.py

--- problems/the-travel/src/tester.py
+++ problems/the-travel/src/tester.py
@@ -1,29 +1,15 @@
 from logging import exception
-from typing import List
+from typing import List, Tuple
 from solution import solve
 import random
 
 
 # brute force
-def solve2(a: List[int]) -> int:
-    N = len(a)
-
-    # precalculation of the array with the accumulated sum
-    accum_sum = [0 for _ in range(N+1)]
-    for i, item in enumerate(a):
-        accum_sum[i+1] = item + accum_sum[i]
-
-    for k in range(1, N+1):
-        # checks if there is any subarray of size K, with non-positive sum
-        l = 0; r = k
-        while r <= N:
-            if accum_sum[r] - accum_sum[l] <= 0:
-                break
-            l += 1; r += 1
-        else:
-            return k
-
-    return -1
+def solve2(n : int, edges : List[Tuple[int,int,int]], Q : List[Tuple[int,int,int]]):
+    
+    #TODO
+    
+    pass
 
 
 def check(a: List[int], k: int) -> int:
@@ -44,36 +30,41 @@ def check(a: List[int], k: int) -> int:
 
     return False
 
+def random_graph(n, prob):
+    edges = []
+    
+    for i in range(n):
+        for j in range(n):
+            if random.random() < prob:
+                edges.append((i,j, random.randint(0,500)))
+                
+    return edges
 
-def tester(amount_tests: int, max_size: int):
+
+def tester(amount_tests: int, max_nodes: int):
     
     for case in range(amount_tests):
         
-        size = random.randint(1, max_size)
+        n = random.randint(1, max_nodes)
         
-        a = []
-        for _ in range(size - size//2):
-            a.append(random.randint(-1e9, 1e9))
+        edges = random_graph(n, 0.5) #TODO: adjust p value
+        Q = random_graph(n, 0.2) #TODO: adjust p value
 
-        a += [random.randint(-1e9, 1e9)] * (size//2)
-
-        k = solve(a)
+        k1 = solve(n, edges, Q)
+        k2 = solve2(n ,edges, Q)
         
         print("\033[1;37m" + f'Case {case}, verdict:')
-        if k > 0 and check(a, k):
-            print( chr(27) + "[1;32m" + "Accepted")
-        elif solve2(a) == k == -1:
+        if k1 == k2:
             print( chr(27) + "[1;32m" + "Accepted")
         else:
             print(chr(27) + "[1;31m" +'Wrong Answer')
-            print(f'k={k} is not solution for the given list.\n')
         print("\033[0;37m" )
 
 if __name__ == '__main__':
-    AMOUNT_OF_TEST_CASES = 1000
-    SIZE_OF_ARRAY = 1000
+    AMOUNT_OF_TEST_CASES = 10
+    MAX_NUMBER_OF_NODES = 1000
 
     tester(
         amount_tests=AMOUNT_OF_TEST_CASES,
-        max_size=SIZE_OF_ARRAY
+        max_nodes=MAX_NUMBER_OF_NODES
     )
